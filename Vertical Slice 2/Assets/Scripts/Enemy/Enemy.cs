84e0c53f786c60c59af06ce54f0e2729f6735af7
Spraxs <jaimy.putter@outlook.com>
2019-01-10 13:53:06 +0100

    Created the Enemy class

---
+++ Vertical Slice 2/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+
+    private GameObject player;
+    private int health;
+    private float attackRange;
+
+    private void Awake()
+    {
+      //  player = GetComponent<>();
+    }
+
+    public abstract void Attack();
+
+    public bool IsPlayerClose()
+    {
+        return Vector2.Distance(player.transform.position, gameObject.transform.position) < attackRange;
+    }
+}
