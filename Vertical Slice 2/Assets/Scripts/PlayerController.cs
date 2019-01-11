6b8b16b98b011a53650ae3b3c10bc6bb5643e4d4
AlexJanson <alex@hoofddorp.nl>
2019-01-11 14:53:36 +0100

    tilemap and collision layer added

---
+++ Vertical Slice 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+	{
+		private SpriteRenderer _sprite;
+		private Rigidbody2D _body;
+		public float MoveSpeed;
+
+		private void Awake()
+		{
+			_body = GetComponent<Rigidbody2D>();
+			_sprite = GetComponent<SpriteRenderer>();
+		}
+
+		private void Update()
+		{
+			var horiz = Input.GetAxis("Horizontal");
+			var vert = Input.GetAxis("Vertical");
+
+			_body.velocity = new Vector2(horiz, vert) * MoveSpeed;
+		}
+	}
