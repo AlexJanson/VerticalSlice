ae3ce714b23bdd8a7fb3fe0adcd54fa32d77f94b
Spraxs <jaimy.putter@outlook.com>
2019-01-14 09:48:32 +0100

    Created cow milk projectile

---
+++ Vertical Slice 2/Assets/Scripts/Enemy/Cow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : Enemy {
+
+    public override void Attack()
+    {
+        if (!CanAttack()) return;
+
+        GameObject projectile = null;
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsPlayerClose())
+        {
+            Attack();
+        }
+	}
+}
