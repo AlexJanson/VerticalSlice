eba8bed4ab6ac851ae409aae220023db8c4ebcfb
AlexJanson <alex@hoofddorp.nl>
2019-01-14 10:54:05 +0100

    Player shooting working

---
+++ Vertical Slice 2/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector2 target;
+    public float speed = 6.0f;
+
+    private void Start()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+        if(Vector2.Distance(target, transform.position) < 0.05f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
