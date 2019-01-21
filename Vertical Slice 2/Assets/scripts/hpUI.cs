d6b0bcc8c9a5a9d562dc889e84c3b2f9ffb434df
Spraxs <jaimy.putter@outlook.com>
2019-01-21 08:56:42 +0100

    Added UI health

--- Vertical Slice 2/Assets/scripts/hpUI.cs
+++ Vertical Slice 2/Assets/scripts/hpUI.cs
@@ -7,32 +7,30 @@ public class hpUI : MonoBehaviour {
 
     public Image HeartFilled;
     public GameObject healthPercentage;
-    private player Player;
-    public GameObject UIscripts;
+    public PlayerDeath playerDeath;
     private float begin;
     static float t = 0.0f;
 
     // Use this for initialization
     void Start () {
+        playerDeath = FindObjectOfType<PlayerDeath>();
         HeartFilled.type = Image.Type.Filled;
         HeartFilled.fillMethod = Image.FillMethod.Vertical;
-        Player = UIscripts.GetComponent<player>();
-        begin = Player.hp;
+        begin = playerDeath.health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Player = UIscripts.GetComponent<player>();
 
-        if(Player.hp != begin) {
+        if(playerDeath.health != begin) {
 
-            HeartFilled.fillAmount = Mathf.Lerp( (begin/100.0f), (Player.hp/100.0f), t);
-            t += 0.5f * Time.deltaTime;
-            healthPercentage.GetComponent<Text>().text = Player.hp.ToString();
+            HeartFilled.fillAmount = Mathf.Lerp( (begin/100.0f), (playerDeath.health / 100.0f), t);
+            t += 1.0f * Time.deltaTime;
+            healthPercentage.GetComponent<Text>().text = "" + Mathf.Round(HeartFilled.fillAmount * 100f);
         }
 
         if (t > 1.0f) {
-            begin = Player.hp;
+            begin = playerDeath.health;
             t = 0.0f;
         }
     }
