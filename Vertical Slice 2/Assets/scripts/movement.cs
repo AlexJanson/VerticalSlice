7a4f6c09f39a880c658b8735395520c07a5099d0
IkBenLiam <39294926+IkBenLiam@users.noreply.github.com>
2019-01-24 23:29:01 +0100

    Polished movement script
    (Honorable mention:  Rosco Armando Roerdinkveldboom :) - 1e jaars developer

--- Vertical Slice 2/Assets/scripts/movement.cs
+++ Vertical Slice 2/Assets/scripts/movement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class movement : MonoBehaviour {
 
     private Rigidbody2D player;
-    public float speed = 1f;
+    public float speed;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		player = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -17,36 +19,43 @@ public class movement : MonoBehaviour {
 
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        float isRight=0;
+        float isBack = 0;
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        
+        if (pz.x > transform.position.x)
+        {
+            isRight = 1;
+        }else if (pz.x < transform.position.x)
+        {
+            isRight = -1;
+        }
+        if (pz.y > transform.position.y+1.5)
+        {
+            isBack = 1;
+        }else if (pz.y < transform.position.y-1.5)
+        {
+            isBack = -1;
+        }
+        animator.SetFloat("Vertical_Walk",v);
+        animator.SetFloat("Horizontal_Walk",h);
+        animator.SetFloat("Vertical_Idle", isBack);
+        animator.SetFloat("Horizontal_Idle", isRight);
+
+        player.velocity = new Vector2(h * speed * Time.deltaTime, player.velocity.y);
+        player.velocity = new Vector2(player.velocity.x, v * speed *Time.deltaTime);
 
 
-        player.AddRelativeForce(Vector2.up * Time.deltaTime * speed * Input.GetAxis("Vertical"));
-        player.AddRelativeForce(Vector2.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
+
+        //player.velocity = Vector2.up * Time.deltaTime * speed * Input.GetAxis("Vertical");
+
+        //player.AddRelativeForce(Vector2.up * Time.deltaTime * speed * Input.GetAxis("Vertical"), ForceMode2D.Impulse);
+        //player.AddRelativeForce(Vector2.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"), ForceMode2D.Impulse);
         if (h == 0f && v == 0f)
         {
             player.velocity = Vector2.zero;
         }
-        //else if (h == 0 && v != 0)
-        //{
-        //    player.velocity = new Vector2(0, player.velocity.y);
-        //    player.AddRelativeForce((Vector2.up * speed) * v / 2);
-        //}
-        //else if (h != 0 && v == 0)
-        //{
-        //    player.AddRelativeForce((Vector2.right * speed) * h / 2);
-        //    player.velocity = new Vector2(player.velocity.x, 0);
-        //}
-
-
-            //if (player.velocity.x != 0f)
-            //{
-            //    player.velocity = new Vector2(player.velocity.x, player.velocity.x);
-            //}
+       
     }
 }
-// is er input op een as die voorheen geen input had?
-// zo ja, zet dan de velocity van deze nieuwe input (as) naar de velocity die er al wel was
-
-// is er al kracht op de x of y waarde v/d velocity?
-// zo ja, pas deze dan toe op de andere as-wanneer deze 0 is
-// zo nee, dan kun je gewoon de force toevoegen
-//
