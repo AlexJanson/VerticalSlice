898dbf60c845e294a7623c168e43df5b4e809aa1
Suryani <17453@ma-web.nl>
2019-01-24 21:42:56 +0100

    waves(placeholder and movement change

--- Vertical Slice 2/Assets/scripts/movement.cs
+++ Vertical Slice 2/Assets/scripts/movement.cs
@@ -22,6 +22,8 @@ public class movement : MonoBehaviour {
         player.AddRelativeForce(Vector2.up * Time.deltaTime * speed * Input.GetAxis("Vertical"));
         player.AddRelativeForce(Vector2.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
 
+        player.velocity = new Vector2(h*speed,v*speed);
+
         if (h == 0f && v == 0f)
         {
             player.velocity = Vector2.zero;
