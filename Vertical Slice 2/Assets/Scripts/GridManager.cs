662c1f3898e622563943226912708296a52ba612
AlexJanson <alex@hoofddorp.nl>
2019-01-25 09:57:45 +0100

    Begin of pathfinding generating grid

---
+++ Vertical Slice 2/Assets/Scripts/GridManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+    
+	void Start () {
+        FindObjectOfType<CreateNodesFromTilemaps>().generateNodes();
+	}
+}
