5e12505bbc78a335b185b11d27e1c86143b3bf1e
AlexJanson <alex@hoofddorp.nl>
2019-02-01 15:53:42 +0100

    Boerin animation

---
+++ Vertical Slice 2/Assets/BoerinAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class BoerinAnimation : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        FindObjectOfType<WaveManager>().startNewWaveAction += PointAnimation;
+    }
+
+    private void PointAnimation(int wave)
+    {
+        anim.Play("Point animation2");
+        Debug.Log("Called");
+    }
+}
